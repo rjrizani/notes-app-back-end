81850283abf9c95e00e0859dad46cd6867be411d
rjrizani <rjrizani@gmail.com>
2023-10-22 12:55:44 +0800

    initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies *//* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
+
+
+const server = Hapi.server({
+  port: 5000,
+  host: 'localhost',
+  routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+});
